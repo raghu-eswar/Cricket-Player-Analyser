5a3b4358b713ed3f0141d0794fc19083294b1fc3
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-21 11:04:56 +0530

    Refactored PlayerDAOBuilder.java to maintain dry principle.

--- src/main/java/IPL/PlayerDAOBuilder.java
+++ src/main/java/IPL/PlayerDAOBuilder.java
@@ -18,42 +18,42 @@ public class PlayerDAOBuilder {
     Map<String, PlayerDAO> playerDataMap = new HashMap<>();
 
     public Map<String, PlayerDAO> loadPlayersData(String batsmanCSVFilePath, String bowlerCSVFilePath) throws PlayerAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(batsmanCSVFilePath))) {
-            ICSVBuilder csvBuilder = CSVBuilderFactory.getCsvBuilder();
-            Iterator<BatsmanCSV> csvIterator = csvBuilder.getCsvIterator(reader, BatsmanCSV.class);
-            Iterable<BatsmanCSV> csvIterable = () -> csvIterator;
-            StreamSupport.stream(csvIterable.spliterator(), false)
-                    .parallel()
-                    .forEach(batsmanCSV -> playerDataMap.put(batsmanCSV.getPlayerName(), new PlayerDAO(batsmanCSV)));
-        } catch (IOException e) {
-            throw new PlayerAnalyserException(e.getMessage(),
-                    PlayerAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
-        } catch (CSVBuilderException e) {
-            throw new PlayerAnalyserException(e.getMessage(), String.valueOf(e.type));
-        }
+        loadBatsmanData(batsmanCSVFilePath);
         loadBowlersData(bowlerCSVFilePath);
         return playerDataMap;
     }
 
-    public void loadBowlersData(String playerCSVFilePath) throws PlayerAnalyserException {
-        try (Reader reader = Files.newBufferedReader(Paths.get(playerCSVFilePath))) {
-            ICSVBuilder csvBuilder = CSVBuilderFactory.getCsvBuilder();
-            Iterator<BowlerCSV> csvIterator = csvBuilder.getCsvIterator(reader, BowlerCSV.class);
-            Iterable<BowlerCSV> csvIterable = () -> csvIterator;
-            StreamSupport.stream(csvIterable.spliterator(), false)
+    private void loadBatsmanData(String batsmanCSVFilePath) throws PlayerAnalyserException {
+        Iterable<BatsmanCSV> csvIterable = getIterable(batsmanCSVFilePath, BatsmanCSV.class);
+        StreamSupport.stream(csvIterable.spliterator(), false)
                     .parallel()
-                    .forEach(bowlerCSV -> {
-                        PlayerDAO playerDAO = playerDataMap.get(bowlerCSV.getPlayerName());
-                        if (playerDAO == null)
-                            playerDataMap.put(bowlerCSV.getPlayerName(), new PlayerDAO(bowlerCSV));
-                        else
-                            playerDAO.setBowlingData(bowlerCSV);
-                    });
+                    .forEach(batsmanCSV -> playerDataMap.put(batsmanCSV.getPlayerName(), new PlayerDAO(batsmanCSV)));
+    }
+
+    private void loadBowlersData(String bowlerCSVFilePath) throws PlayerAnalyserException {
+        Iterable<BowlerCSV> csvIterable = getIterable(bowlerCSVFilePath, BowlerCSV.class);
+        StreamSupport.stream(csvIterable.spliterator(), false)
+                            .parallel()
+                            .forEach(bowlerCSV -> {
+                                PlayerDAO playerDAO = playerDataMap.get(bowlerCSV.getPlayerName());
+                                if (playerDAO == null)
+                                    playerDataMap.put(bowlerCSV.getPlayerName(), new PlayerDAO(bowlerCSV));
+                                else
+                                    playerDAO.setBowlingData(bowlerCSV);
+                            });
+    }
+
+    private <T> Iterable<T> getIterable(String path, Class<T> csvClass) throws PlayerAnalyserException {
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+            ICSVBuilder csvBuilder = CSVBuilderFactory.getCsvBuilder();
+            Iterator<T> csvIterator = csvBuilder.getCsvIterator(reader, csvClass);
+            return () -> csvIterator;
+        } catch (CSVBuilderException e) {
+            throw new PlayerAnalyserException(e.getMessage(), String.valueOf(e.type));
         } catch (IOException e) {
             throw new PlayerAnalyserException(e.getMessage(),
                     PlayerAnalyserException.ExceptionType.CSV_FILE_PROBLEM);
-        } catch (CSVBuilderException e) {
-            throw new PlayerAnalyserException(e.getMessage(), String.valueOf(e.type));
         }
     }
 
